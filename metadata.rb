ae2252f4517a8311db6dac4fce583d64fe9786a2
Haggai Philip Zagury <haggai.zagury@sap.com>
2014-01-16 21:36:43 +0200

    Update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hagzag@lightapp.com'
 license          'Apache 2.0'
 description      'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w{ php apache2 mysql yum apt line ark yum-epel iptables }.each do |cb|
   depends cb
